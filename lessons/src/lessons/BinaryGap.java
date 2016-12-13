ce02b0a5e2a2ab8710f10b8334c9705b096c0982
Ihor Ilkevych <iilkev@softserveinc.com>
2016-12-13 14:58:34 +0200

    BinaryGap

--- lessons/src/lessons/BinaryGap.java
+++ lessons/src/lessons/BinaryGap.java
@@ -2,4 +2,22 @@ package lessons;
 
 public class BinaryGap {
 
+	public int solution(int N) {
+		int count = 0, max = 0;
+		boolean found = false;
+		
+		while(N > 0) {
+			if((N & 1) == 1){
+				if(count > max){
+					max = count;
+				}
+				count = 0;
+				found = true;
+			} else if (found) {
+				count++;
+			}
+			N = N >> 1;
+		}
+		return max;
+	}
 }
