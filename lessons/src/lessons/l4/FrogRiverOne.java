f0c5d4b203879616905fb468983879b7013d86fe
Ihor Ilkevych <iilkev@softserveinc.com>
2016-12-19 15:06:18 +0200

    FrogRiverOne

---
+++ lessons/src/lessons/l4/FrogRiverOne.java
@@ -0,0 +1,17 @@
+package lessons.l4;
+
+public class FrogRiverOne {
+	public int solution(int A[]) {
+		int xor = 0;
+		
+		for (int i = 0; i < A.length; i++) {
+			if(A[i] > A.length)
+				return 0;
+			else {
+				xor ^= i + 1;
+				xor ^= A[i];
+			}
+		}
+		return xor == 0? 1:0;
+	}
+}
