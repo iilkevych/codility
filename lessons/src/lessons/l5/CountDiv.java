2426c12fe977b68ef5019c7c2650d981240b1814
Ihor Ilkevych <iilkev@softserveinc.com>
2016-12-19 18:30:51 +0200

    CountDiv

---
+++ lessons/src/lessons/l5/CountDiv.java
@@ -0,0 +1,10 @@
+package lessons.l5;
+
+public class CountDiv {
+	public int solution(int A, int B, int K) {
+		if (A % K == 0)
+			return (B / K) - ((A / K) - 1);
+		else
+			return (B / K) - (A / K);
+	}
+}
